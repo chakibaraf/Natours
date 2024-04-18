0c0461cd3e56936d24c4c6a0b7cff0abe97c2ba6
unknown <araf.chakib.m@gmail.com>
2024-04-19 00:02:12 +0200

    api restruc

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,22 +1,62 @@
 const mongoose = require('mongoose')
 
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'A name must have a name'],
-      unique:true
-    },
-    rating: {
-  
-      type: Number,
-      default: 4.5,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price']
-    }
-  })
-  const Tour = mongoose.model('Tour',tourSchema);
+  name: {
+    type: String,
+    required: [true, 'A name must have a name'],
+    unique: true
+  },
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have duration'],
 
- 
-module.exports   = Tour;
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have group size'],
+
+  },
+  difficult: {
+    type: Number,
+    required: [true, 'A tour must have difficult'],
+  },
+  ratingAverage: {
+
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summmery: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have description']
+
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startedDate:[Date],
+
+})
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+module.exports = Tour;

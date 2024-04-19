5b30fb0207bf92fc0478a382c23f1c8ff1906418
unknown <araf.chakib.m@gmail.com>
2024-04-20 00:30:43 +0200

    function redfil

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,30 +1,35 @@
 
 
-const Tour = require('../models/tourModel')
-
-
-
-
-
+const Tour = require('../models/tourModel');
 
 
 exports.getAllTours = async (req, res) => {
-    try{
+    try {
 
-        const tours = await Tour.find();
-        
+       //BUILD QUERY
+        const queryObj = { ...req.query };
+        const excludedFields = ['page','sort','limit','fields']
+        excludedFields.forEach(el => delete queryObj[el])
+        console.log(req.query , queryObj);
+
+        const query = await Tour.find( queryObj);
+
+        //CONST EXECUTE QUERY
+        const tours = await query ;
+
+        //SEND RESPONSE
         res.status(200).json({
-            status:'succes',
-            message:'all tours',
-            results:tours.length,
-            data:{
+            status: 'succes',
+            message: 'all tours',
+            results: tours.length,
+            data: {
                 tours: tours
             }
         })
-    } catch(err){
+    } catch (err) {
         res.status(404).json({
-            status:'fail',
-            message:err
+            status: 'fail',
+            message: err
         })
 
 
@@ -33,57 +38,55 @@ exports.getAllTours = async (req, res) => {
 
 // requete pour avoir un utilisateur 
 exports.getTour = async (req, res) => {
-    try{
+    try {
 
         const tour = await Tour.findById(req.params.id);
-        
+
         res.status(200).json({
-            status:'succes',
-            message:'good id',
-            
-            data:{
-                 tour
+            status: 'succes',
+            message: 'good id',
+
+            data: {
+                tour
             }
         });
-        
-    }catch(err){
+
+    } catch (err) {
         res.status(404).json({
-            status:'fail',
-            message:err
+            status: 'fail',
+            message: err
         })
     }
 
-} 
+}
 
 exports.updateTour = async (req, res) => {
-    try{
+    try {
 
         const updt = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-                new:true,
-                runValidators:true
+            new: true,
+            runValidators: true
         });
-        
+
         res.status(200).json({
-            
+
             status: 'success',
             data: {
                 updt
             }
         });
-    }catch(err){
+    } catch (err) {
         res.status(404).json({
-            status:'fail',
-            message:err
+            status: 'fail',
+            message: err
         })
 
     }
-        
+
 }
 
 exports.createTour = async (req, res) => {
     try {
-
-        
 
         const newTour = await Tour.create(req.body)
 
@@ -97,36 +100,33 @@ exports.createTour = async (req, res) => {
         });
     } catch (err) {
         res.status(400).json({
-            status:"fail",
-            message:err
+            status: "fail",
+            message: err
         })
     }
-
-
-
-
 
 
 }
 
 exports.deleteTour = async (req, res) => {
-try{
+    try {
 
-    const deleteT = await Tour.findByIdAndDelete(req.params.id)
-    
-    res.status(204).json({
-        status: 'success',
-        message: 'delete tour',
-        data:{
-            delete:deleteT
-        }
-    })
-}catch(err){
-    res.status(404).json({
-        status:'fail',
-        message:err,
-        
-    })
-}
+        const deleteT = await Tour.findByIdAndDelete(req.params.id)
+
+        res.status(204).json({
+            status: 'success',
+            message: 'delete tour',
+            data: {
+                delete: deleteT
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+
+        })
+    }
+
 
 }
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-const Tour = require("../../models/tourModel")
+const dotenv = require('dotenv');
+const Tour = require("../../models/tourModel");
 
 
 
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path:'./config.env'});
 
 const DB = process.env.DATABASE.replace('<PASSWORD>',
     process.env.DATABASE_PASSWORD);
 
-// const DB_LOCAL = process.env.DATABASE_LOCAL;
+// const DB_LOCAL = process.env.DATABASE_LOCAL.
 
-mongoose.connect(DB, {
+mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
-}).then(() => console.log('db connection success'))
+}).then(() => console.log('db connection success'));
 
-//READ JSON FILE
+//READ JSON FILE.
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
 
-//IMPORT DATA INTO DB
+//IMPORT DATA INTO DB.
 
 const importData = async () => {
     try {
@@ -31,25 +31,25 @@ const importData = async () => {
         console.log(('data successfully loaded'))
         process.exit();
     } catch (err) {
-        console.log(err)
+        console.log(err);
 
     }
 }
 
-// DELETE ALL DATA FROM COLLECTION  
+// DELETE ALL DATA FROM COLLECTION.
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
         console.log('data successfuly deleted');
         process.exit();
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
 }
 if(process.argv[2] === '--import'){
     importData()
 }else if (process.argv[2]=== '--delete'){
-deleteData()
-}
+deleteData();
+};
 
-console.log(process.argv)
+console.log(process.argv);

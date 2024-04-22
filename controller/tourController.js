eb9e42f2828078476695f1925ad383f092fb1f60
unknown <araf.chakib.m@gmail.com>
2024-04-22 17:42:10 +0200

    function filter

--- controller/tourController.js
+++ controller/tourController.js
@@ -6,34 +6,68 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
     try {
 
-       //BUILD QUERY
+        //BUILD QUERY
+        // 1A) filtering Filtrage des champs indésirables dans l'objet de requête
         const queryObj = { ...req.query };
-        const excludedFields = ['page','sort','limit','fields']
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
         excludedFields.forEach(el => delete queryObj[el])
-        console.log(req.query , queryObj);
+        console.log(req.query, queryObj);
 
-        const query = await Tour.find( queryObj);
+        // 1B) advanced query Requête avancée avec opérateurs de comparaison ($gte, $gt, $lte, $lt)
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
 
-        //CONST EXECUTE QUERY
-        const tours = await query ;
-
-        //SEND RESPONSE
-        res.status(200).json({
-            status: 'succes',
-            message: 'all tours',
-            results: tours.length,
-            data: {
-                tours: tours
-            }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
+        // {difficulty : 'easy', duration : {gte : 5}}
+        let query = Tour.find(JSON.parse(queryStr));
 
 
+        // 2) SORTING  TRI
+        // Si la requête contient une directive de tri
+        if (req.query.sort) {
+
+            const sortBy = req.query.sort.split(',').join(' ');
+            // Séparation des champs de tri par une virgule et les convertir en chaîne pour la fonction de tri
+            console.log(sortBy);
+            // Appliquer le tri à la requête
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt')
+        
+
+        // sort('price ratingAverage');
     }
+
+    //3) field limiting
+        
+    if(req.query.fields){
+        const fields = req.query.fields.split(',').join(' ');
+        query = query.select(fields);
+    } else{
+        query = query.select('-__v')
+    }
+        //CONST EXECUTE QUERY
+        const tours = await query;
+
+    //SEND RESPONSE
+    res.status(200).json({
+        status: 'succes',
+        message: 'all tours',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
+
+} catch (err) {
+    res.status(404).json({
+        status: 'fail',
+        message: err
+    })
+
+
+}
+
 }
 
 // requete pour avoir un utilisateur 

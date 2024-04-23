1e187840d103b0c060a7d146b4ce5ac70c59ee5e
unknown <araf.chakib.m@gmail.com>
2024-04-23 16:59:43 +0200

    function filter

--- controller/tourController.js
+++ controller/tourController.js
@@ -33,40 +33,53 @@ exports.getAllTours = async (req, res) => {
             query = query.sort(sortBy);
         } else {
             query = query.sort('-createdAt')
-        
 
-        // sort('price ratingAverage');
-    }
 
-    //3) field limiting
-        
-    if(req.query.fields){
-        const fields = req.query.fields.split(',').join(' ');
-        query = query.select(fields);
-    } else{
-        query = query.select('-__v')
-    }
+            // sort('price ratingAverage');
+        }
+
+        //3) field limiting
+
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
+        // 4) pagination
+
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.sort({ _id: 1 }).skip(skip).limit(limit);
+
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('this page does not exist');
+        }
         //CONST EXECUTE QUERY
         const tours = await query;
 
-    //SEND RESPONSE
-    res.status(200).json({
-        status: 'succes',
-        message: 'all tours',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-    });
+        //SEND RESPONSE
+        res.status(200).json({
+            status: 'succes',
+            message: 'all tours',
+            results: tours.length,
+            data: {
+                tours: tours
+            }
+        });
 
-} catch (err) {
-    res.status(404).json({
-        status: 'fail',
-        message: err
-    })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
 
 
-}
+    }
 
 }
 
